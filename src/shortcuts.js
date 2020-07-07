33e46a862eddc124fe10795710093315716a8dee
Andy O'Neill <aoneill01@gmail.com>
2020-07-07 09:34:45 -0400

    Clean up commands

--- src/shortcuts.js
+++ src/shortcuts.js
@@ -1,25 +1,24 @@
 const CTRL = "Ctrl"
 const CMD = "⌘"
 const TAB = "⇥"
-const SHIFT = "⇧"
+// const SHIFT = "⇧"
 const SPACE = "\xa0\xa0\xa0\xa0\xa0\xa0"
 
 export default [
-  {
-    name: "Vim",
-    commands: [
-      { keys: [CTRL, "V"], desc: "Block select mode" },
-      { keys: ['"', "A", "Y"], desc: "Copy selected text to register A" },
-      { keys: ["Q", "A", "…", "Q"], desc: "Record macro A" },
-      { keys: ["@", "A"], desc: "Play macro A" },
-    ],
-  },
   {
     name: "MacOS",
     commands: [
       { keys: [CMD, CTRL, SPACE], desc: "Emoji keyboard" },
       { keys: [CMD, TAB], desc: "Switch programs" },
-      { keys: [CTRL, "`"], desc: "Switch instance of current program" },
+      { keys: [CMD, "`"], desc: "Switch instance of current program" },
+    ],
+  },
+  {
+    name: "Vim",
+    commands: [
+      { keys: ['"', "A", "Y"], desc: "Copy selected text to register A" },
+      { keys: ["Q", "A", "…", "Q"], desc: "Record macro A" },
+      { keys: ["@", "A"], desc: "Play macro A" },
     ],
   },
   {
@@ -27,8 +26,6 @@ export default [
     commands: [
       { keys: [CTRL, TAB], desc: "Recently used files" },
       { keys: [CTRL, "`"], desc: "Switch between terminal" },
-      { keys: [CMD, "P"], desc: "Find file" },
-      { keys: [SHIFT, CMD, "P"], desc: "Command palette" },
     ],
   },
 ]
